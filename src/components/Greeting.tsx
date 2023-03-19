4db609d91e009e30bc8b0d40bf76cf7a7e407f8d
Anirban20001962 <anirbandhakuria@gmail.com>
2023-03-19 09:01:02 +0530

    completed v1

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,19 @@
+import { FC, useState } from 'react';
+
+const Greeting: FC<{ messages: string[] }> = ({ messages }) => {
+	const randomMessage = () =>
+		messages[Math.floor(Math.random() * messages.length)];
+
+	const [greeting, setGreeting] = useState(randomMessage());
+
+	return (
+		<div>
+			<h3>{greeting}! Thank you for visiting</h3>
+			<button onClick={() => setGreeting(randomMessage())}>
+				New Greeting
+			</button>
+		</div>
+	);
+};
+
+export default Greeting;
